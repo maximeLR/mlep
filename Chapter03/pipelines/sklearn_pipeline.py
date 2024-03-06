ba6e3adf96620ec75bfd4e22206a586d75af40e9
maximeLR <maxime.leroch@gmail.com>
2024-03-06 16:05:37 +0100

    Work on pipelines

--- Chapter03/pipelines/sklearn_pipeline.py
+++ Chapter03/pipelines/sklearn_pipeline.py
@@ -3,6 +3,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 numeric_features = ["age", "balance"]
 numeric_transformer = Pipeline(
@@ -34,4 +36,27 @@ clf_pipeline = Pipeline(
     steps=[("preprocessor", preprocessor), ("classifier", LogisticRegression())]
 )
 
+df = pd.read_csv("pipelines/data/bank/bank.csv", delimiter=";", decimal=",")
+# Assume there was some EDA and feature analysis to select below
+feature_cols = [
+    "age",
+    "balance",
+    "job",
+    "marital",
+    "education",
+    "contact",
+    "housing",
+    "loan",
+    "default",
+    "day",
+]
+
+# Features and target
+X = df[feature_cols].copy()
+y = df["y"].apply(lambda x: 1 if x == "yes" else 0).copy()
+
+# Train/test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 clf_pipeline.fit(X_train, y_train)
